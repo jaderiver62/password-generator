79db0aab6a6f68a016eb1305fc6cde21d8b27e03
Nina Cummings <67253353+jaderiver62@users.noreply.github.com>
2020-12-16 08:08:20 -0600

    Update script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,33 @@
 // Assignment code here
-
-
+var startPasswordGenerater = function() {
+    var confirmationCheck = confirm("Do you want to generate a password?");
+    if(confirmationCheck){
+        var passwordLength = prompt("How long does the password need to be? (must be between 8 and 128 characters long) ");
+        var numberLowercase = prompt("How many lowercase letters should be generated?");
+        var numberUppercase = prompt("How many uppercase letters should be generated?");
+        var numberNumeric = prompt("How many characters should be numeric?");
+        var numberSpecialCharacter = prompt("How many special characters should be generated?");
+    } else { 
+        alert("Good Bye!");
+    }
+    
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+    var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+    function writePassword() {
+        var password = generatePassword();
+        var passwordText = document.querySelector("#password");
 
-    passwordText.value = password;
+        passwordText.value = password;
 
-}
+    }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+    generateBtn.addEventListener("click", writePassword);
+    }
 // Click on the button to generate a password
-
+startPasswordGenerater();
 
 // Prompt user for password criteria which use can select and customize
 // Prompt options: lowercase, uppercase, numeric, and/or special characters
