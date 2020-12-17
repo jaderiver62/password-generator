812406eadb6ccc2290f91bfe229cfe06ad2bb778
Nina Jade <jaderiver64@gmail.com>
2020-12-17 00:37:02 -0600

    random passowrd genterator code and output first draft

--- Develop/script.js
+++ Develop/script.js
@@ -5,6 +5,8 @@ function writePassword() {
     var passwordText = document.querySelector("#password");
     // Write password to the #password input
     passwordText.value = password;
+    alert("Generated Password: " + password);
+
 
 }
 
@@ -13,45 +15,88 @@ generateBtn.addEventListener("click", writePassword);
 
 // Prompt user for password criteria which user can select and customize
 var generatePassword = function() {
-    // Code validates input - Criteria: at least one character type should be selected and all prompts should be answered
-    // Prompts user for password length - between 8 and 128 characters
-    var passwordLength = prompt("How long does the password need to be? (must be between 8 and 128 characters long)", 8);
-    passwordLength = validateNumberInput(passwordLength);
-    // Prompt options: lowercase, uppercase, numeric, and/or special characters
-    alert("Passwords are required to have at least one character type")
-    var numberLowercase = prompt("How many lowercase letters should be generated?", 3);
-    numberLowercase = validateNumberInput(numberLowercase);
-    ensureCapacity(passwordLength, numberLowercase);
-    var totalNumber = passwordLength + numberLowercase;
-    var numberUppercase = prompt("How many uppercase letters should be generated?", 2);
-    numberUppercase = validateNumberInput(numberUppercase);
-    totalNumber += numberUppercase;
-    ensureCapacity(totalNumber, numberUppercase);
-    var numberNumeric = prompt("How many characters should be numeric?", 2);
-    numberNumeric = validateNumberInput(numberNumeric);
-    totalNumber += numberNumeric;
-    ensureCapacity(totalNumber, numberNumeric);
-    var numberSpecialCharacter = prompt("How many special characters should be generated?", 1);
-    totalNumber += numberSpecialCharacter;
-    numberSpecialCharacter = validateNumberInput(numberSpecialCharacter);
-    if (totalNumber > passwordLength || totalNumber <= 0) {
-        alert("Invalid selection, try again");
-        generatePassword();
-    } else { return passwordGenerator(passwordLength, numberLowercase, numberUppercase, numberNumeric, numberSpecialCharacter); }
-
-
-    var validateNumberInput = function(string) {
-        var number = parseInt(string);
-        if (!number.isInteger() || number < 8 || number > 128) {
-            alert("Invalid input: Enter a positive digit between 8 and 128!")
-            generatePassword();
-        } else { return number; }
-    }
-    var ensureCapacity = function(currentTotal, newNumber) {
-        if (newNumber > currentTotal || newNumber > passwordLength) {
+        // Code validates input - Criteria: at least one character type should be selected and all prompts should be answered
+        // Prompts user for password length - between 8 and 128 characters
+        var passwordLength = prompt("How long does the password need to be? (must be between 8 and 128 characters long)", 8);
+        passwordLength = validateNumberInput(passwordLength);
+        // Prompt options: lowercase, uppercase, numeric, and/or special characters
+        alert("Passwords are required to have at least one character type")
+        var numberLowercase = prompt("How many lowercase letters should be generated?", 3);
+        numberLowercase = validateNumberInput(numberLowercase);
+        ensureCapacity(passwordLength, numberLowercase);
+        var totalNumber = passwordLength + numberLowercase;
+        var numberUppercase = prompt("How many uppercase letters should be generated?", 2);
+        numberUppercase = validateNumberInput(numberUppercase);
+        totalNumber += numberUppercase;
+        ensureCapacity(totalNumber, numberUppercase);
+        var numberNumeric = prompt("How many characters should be numeric?", 2);
+        numberNumeric = validateNumberInput(numberNumeric);
+        totalNumber += numberNumeric;
+        ensureCapacity(totalNumber, numberNumeric);
+        var numberSpecialCharacter = prompt("How many special characters should be generated?", 1);
+        totalNumber += numberSpecialCharacter;
+        numberSpecialCharacter = validateNumberInput(numberSpecialCharacter);
+        if (totalNumber > passwordLength || totalNumber <= 0) {
             alert("Invalid selection, try again");
             generatePassword();
+        } else { return randomPasswordGenerator(passwordLength, numberLowercase, numberUppercase, numberNumeric, numberSpecialCharacter); }
+
+
+        var validateNumberInput = function(string) {
+            var number = parseInt(string);
+            if (!number.isInteger() || number < 8 || number > 128) {
+                alert("Invalid input: Enter a positive digit between 8 and 128!")
+                generatePassword();
+            } else { return number; }
         }
+        var ensureCapacity = function(currentTotal, newNumber) {
+            if (newNumber > currentTotal || newNumber > passwordLength) {
+                alert("Invalid selection, try again");
+                generatePassword();
+            }
+        }
+    }
+    // Produces the requested "random"" elements
+var randomPasswordGenerator = function(length, lowercase, uppercase, numeric, specialCharacter) {
+    var result = '';
+    var upperCharacterSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var lowercaseCharacterSet = 'abcdefghijklmnopqrstuvwxyz';
+    var numberNumeric = '';
+    var specialCharacterSet = "!\"#$%&\'()*+,-./:;<=>?@[\ ]^_`{}|~";
+    if (lowercase > 0) {
+        for (var i = 0; i < lowercase; i++) {
+            result += lowercaseCharacterSet.charAt(getRandomNumber(lowercaseCharacterSet.length));
+        }
+    }
+    if (upperCharacters > 0) {
+        for (var i = 0; i < uppercase; i++) {
+            result += uppercaseCharacterSet.charAt(getRandomNumber(uppercaseCharacterSet.length));
+        }
+    }
+    if (numberNumeric > 0) {
+        for (var i = 0; i < numeric; i++) {
+            result += getRandomNumber(10);
+        }
+    }
+    if (specialCharacter > 0) {
+        for (var i = 0; i < specialCharacter; i++) {
+            result += specialCharacterSet.charAt(getRandomNumber(specialCharacterSet.length));
+        }
+    }
+    if (result.length < length) {
+        var difference = length - result.length;
+        for (var i = 0; i < difference; i++) {
+            result += getRandomNumber(10);
+        }
+    } else if (result.length > length) {
+        alert("Invalid input: Something went wrong!  Try again!");
+        generatePassword();
+    } else { return result; }
+
+
+
+    var getRandomNumber = function(max) {
+        return Math.floor(Math.random() * Math.floor(max));
     }
 }
 
